23a40ca62cfd9ac5b677ab04cb3a63e27612e5a5
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-01 16:03:30 +0100

    added function to remove an element with a button

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 //skapar en regissör som ett HTML-element 
 function renderDirector (director){
     let div = document.createElement("div")
-    div.id = "director"
+    div.id = director.id
+    div.classList.add("director")
     div.innerHTML = `
         <li></li>
         <div>${director.name}.</div>
@@ -11,9 +12,17 @@ function renderDirector (director){
         <div>${director.livingStatus}.</div>
         <div>${director.directorialCredits}.</div>
         <div>${director.numberOfAwards}.</div>
-        <button type="button">Remove</button>
+        <button type="button" id="remove">Remove</button>
         `
     return div
+}
+
+//sett event listener på alla knapparsom ska ta bort ett objekt/element
+function removeDirectorHandelers () {
+  let buttons = document.querySelectorAll('.director button')
+  for (let button of buttons) {
+    button.addEventListener('click', removeDirectorClick)
+  }
 }
 
 //renderar alla regissörer i en 
@@ -24,8 +33,10 @@ function renderDirectors (array){
         let directorElement = renderDirector(director)
         directorsElement.appendChild(directorElement)
     }
+    removeDirectorHandelers()
 }
 
+//lägger till en regissör till databasen
 function directorToDatabase (array, director){
     array.push(director)
 }
@@ -65,10 +76,30 @@ function directorSubmit (event){
   
   }
 
+  //funktionen sätter en eventlistener på knappen add
   function setDirectorHandeler () {
     let button = document.getElementById('add')
     button.addEventListener('click', directorSubmit)
   }
+
+  //tar bort en regissör baserat på dess id
+  function removeDirectorById(array, id){
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id == id) {
+        array.splice(i, 1)
+      }
+  }
+  }
+
+  //använder sig av en regissörs id för at ta ort den från databasen
+  function removeDirectorClick (event) {
+    let button = event.target
+    let id = button.parentElement.id
+    removeDirectorById(database, id)
+    renderDirectors(database)
+  }
+
+
 
 renderDirectors(database)
 setDirectorHandeler()
