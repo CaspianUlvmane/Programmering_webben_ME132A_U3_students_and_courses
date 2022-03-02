cc3eda4f34767cc78cb63365347aac95fe36d819
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-02 13:01:07 +0100

    added alerts and confirm prompts
    Now the site will alert a user when an input field is empty or ask the user to confirm when deleting a director from the database

--- index.js
+++ index.js
@@ -59,13 +59,42 @@ function directorSubmit (event){
     event.preventDefault()
   
     let name = document.getElementById("name").value
+
+    //kollar att användaren har lagt till ett namn
+    if(name == ""){
+      alert("A name is required to add a director to the database")
+    }
+    //tar namnet och omvandlar så att första bokstaven är stor
+    //oavsett vad användaren skrev in i formuläret 
+    let nameSplit = name.split(" ")
+    for (let i = 0; i < nameSplit.length; i++) {
+      nameSplit[i] = nameSplit[i][0].toUpperCase() + nameSplit[i].substr(1);
+    }
+    let nameJoin = nameSplit.join(" ");
+    //tar statusen och omvandlar första bokstaven till versal
     let status = document.getElementById("livingStatus").value
+
+    //kontrollerar att användaren har lagt till en status
+    if(status == ""){
+      alert("A living status is required to add a director to the database")
+    }
+    let statuscapital = status[0].toUpperCase() + status.substring(1);
+    
     let credits = Number(document.getElementById("credits").value)
     let numberOfAwards = Number(document.getElementById("numberOfAwards").value)
     let age = Number(document.getElementById("age").value)
-  
-    let director = newDirector(name, age, status, credits, numberOfAwards)
-  
+
+    //kontrollerar att användaren har lagt till ålder, cred och priser
+    if(age == "0"){
+      alert("An age is required to add a director to the database")
+    }  if(credits == "0"){
+      alert("An amount of credits is required to add a director to the database")
+    }   if(numberOfAwards == "0"){
+      confirm("Are you sure the director has 0 awards?")
+    }
+    
+    let director = newDirector(nameJoin, age, statuscapital, credits, numberOfAwards)
+    
     director.id = database[database.length - 1].id + 1;
   
     directorToDatabase(database, director)
@@ -91,13 +120,18 @@ function directorSubmit (event){
   }
   }
 
-  //använder sig av en regissörs id för at ta ort den från databasen
+  //använder sig av en regissörs id för at ta bort den från databasen
   function removeDirectorClick (event) {
     let button = event.target
     let id = button.parentElement.id
+    let name = button.parentElement.firstChild.nextSibling.nextSibling.nextSibling.textContent
+    console.log(name)
+    let isSure = confirm(`Are you sure you want to remove ${name}`)
+    if (isSure){
     removeDirectorById(database, id)
     renderDirectors(database)
   }
+}
 
   function getDirectorsByAge (array, age) {
     let ageArray = []
@@ -114,8 +148,44 @@ function directorSubmit (event){
     event.preventDefault()
     let age = document.getElementById("filter-age").value
     let directors = getDirectorsByAge(database, age)
-    renderDogs(directors)
+    renderDirectors(directors)
     document.getElementById("filter-age").value = ""
+  }
+
+  function getDirectorsByStatus (array, status) {
+    let statusArray = []
+    for (let director of array) {
+      if (director.livingStatus.toLowerCase() == status.toLowerCase()) {
+        statusArray.push(director)
+      }
+    }
+    return statusArray
+  }
+
+  function filterByStatus(event){
+    event.preventDefault()
+    let status = document.getElementById("filter-status").value
+    let directors = getDirectorsByStatus(database, status)
+    renderDirectors(directors)
+    document.getElementById("filter-status").value = ""
+  }
+
+  function getDirectorsByCredits (array, credits) {
+    let creditsArray = []
+    for (let director of array) {
+      if (director.directorialCredits == credits) {
+        creditsArray.push(director)
+      }
+    }
+    return creditsArray
+  }
+
+  function filterByCredits(event){
+    event.preventDefault()
+    let credits = document.getElementById("filter-credits").value
+    let directors = getDirectorsByCredits(database, credits)
+    renderDirectors(directors)
+    document.getElementById("filter-credits").value = ""
   }
   
   function onShowAllClick(){
@@ -123,11 +193,13 @@ function directorSubmit (event){
   }
   
   function filterDirectorHandelers (){
-    //let breedFilter = document.getElementById("filter-by-breed")
-    let ageFilter = document.getElementById("filter-age")
+    let statusFilter = document.getElementById("statusFilter")
+    let creditsFilter = document.getElementById("creditsFilter")
+    let ageFilter = document.getElementById("ageFilter")
     let showAll = document.getElementById("show-all")
   
-    //breedFilter.addEventListener("submit", onFilterByBreedSubmit)
+    statusFilter.addEventListener("submit", filterByStatus)
+    creditsFilter.addEventListener("submit", filterByCredits)
     ageFilter.addEventListener("submit", filterByAge)
     showAll.addEventListener("click", onShowAllClick)
   }
