4c38eb8dd58fee823e16c652c040c44e6ee88fe2
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-23 16:31:49 +0100

    rendercourse function ads name of correct course

--- students.js
+++ students.js
@@ -1,5 +1,7 @@
 let allStudents = DATABASE.students
 
+let student = [DATABASE.students[0], DATABASE.students[1]]
+
 function renderStudent(id){
     let div = document.createElement("div")
     let student = DATABASE.students[id]
@@ -32,8 +34,6 @@ function completeCredits (student){
 function renderStudents (students){
     let studentsElement =     document.getElementById("wrapper")
     for ( let student of students){
-        console.log(student.studentID)
-        console.log(renderStudent(student.studentID))
         let studentElement = renderStudent(student.studentID)
         studentsElement.appendChild(studentElement)
     }
@@ -43,20 +43,19 @@ function renderStudents (students){
 function renderCourses (student){
     let courseData = DATABASE.courses
     let courses = []
-    for ( let course of student.courses){
-        if (course.courseID == courseData.courseID)
-        courses.push(courseData)
+    for ( let i = 0; i < student.courses.length; i++){
+        let id = student.courses[i].courseId
+        courses.push(courseData[id])
+        
     }
+    console.log(courses)
     let courseDiv = []
-    for (let course of courses){
+    for (let i= 0; i < courses.length; i++){
         let div = document.createElement("div")
-        div.innerHTML =
-        `<h4>${course.title}</h4>
-        <p> ( 0f  credits)</p>`
-        courseDiv.push(div)
+        let text = div.innerHTML =
+        `<div><h4>${courses[i].title}</h4>
+        <p> ( 0f  credits)</p></div>`
+        courseDiv.push(text)
     }
-    for (let i = 0; i < courseDiv.length; i++){
-        let div = courseDiv[i]
-        div.
-    }
+    return courseDiv.toString().split(",").join(" ");
 }
