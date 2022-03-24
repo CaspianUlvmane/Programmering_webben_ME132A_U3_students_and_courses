2adcfea660f6bc3c6cae0fd8ed7be99d53d290f8
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-24 10:50:58 +0100

    eventlistener and function, filters by form

--- filterstudents.js
+++ filterstudents.js
@@ -1,8 +1,22 @@
-function studentLastname (students){
+function studentLastname (){
     let studentsArray = []
-    for ( let student of students){
-        if (lastname.includes(searchWord)) {
-            studentsArray.push(student);
+    for ( let i = 0; i < allStudents.length; i++){
+        document.querySelector("#wrapper").innerHTML = ""
+        console.log(allStudents[i].lastName)
+        console.log(searchWord)
+        if (allStudents[i].lastName.toLocaleLowerCase().includes(searchWord())) {
+            studentsArray.push(allStudents[i]);
+        } if (searchWord() == ""){
+            document.querySelector("#wrapper").innerHTML = ""
         }
+
+    }
+    renderStudents(studentsArray)
 }
+
+function searchWord (){
+    let search = document.getElementById("lastname")
+    return search.value
 }
+
+document.getElementById("lastname").addEventListener("keyup", studentLastname)
