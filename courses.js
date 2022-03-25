a634a253ee2909133cc0621ca02693264ebe6666
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-25 15:26:23 +0100

    created styling and function to generate html

--- courses.js
+++ courses.js
@@ -1,20 +1,80 @@
 let allCourses = DATABASE.courses
+let allTeachers = DATABASE.teachers
+let allStudents = DATABASE.students
 
 let twoCourses = [allCourses[0], allCourses[1]]
 
-function renderCourses(id){
+function renderCourse(id){
     let div = document.createElement("div")
-    let course = DATABASE.courses[id]
     div.id = "container"
     div.innerHTML =
-    `<header>${course.title} (${course.totalCredits} credits)</header>
-    <div>
-        <div id="course">
-            <h3>Courses:</h3>
-            <div id="courses">
+    ` <div id="course">
+    <header>${courseTitle(id)} (${courseTotalCredits(id)} credits)</header>
+    <div id="faculty">
+        <div id="responsible">
+            <h3>Course responsible:</h3>
+            <p>${courseResponsible(id)}</p>
+        </div>
+        <div id="teachers">
+            <h3>Teachers:</h3>
+            <div>
+                ${allCourseTeachers(id)}
             </div>
         </div>
-    </div>`
-    document.querySelector("#siteWrapper").appendChild(div)
+    </div>
+    <h3>Students:</h3>
+    <div id="students">
+        ${allCourseStudents(id)}
+    </div>
+    </div>
+    `
+    document.querySelector("#wrapper").appendChild(div)
     return div
+}
+
+function courseTitle (id){
+    let course = DATABASE.courses[id]
+    return course.title
+}
+
+function courseTotalCredits (id) {
+    let course = DATABASE.courses[id]
+    return course.totalCredits
+}
+
+function courseResponsible (id) {
+    let course = DATABASE.courses[id]
+    let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`)
+    let res = course.courseResponsible
+    return teachersNames[res]
+}
+
+function allCourseTeachers(id){
+    let course = DATABASE.courses[id]
+    let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`)
+    let teachers = []
+    for ( let i = 0; i < teachersNames.length; i++){
+        if (course.teachers.some((value) => value == i)){
+        let div = document.createElement("div")
+        let content = div.innerHTML = `<p>${teachersNames[i]}</p>`
+        teachers.push(content)
+        }
+    }
+
+    return teachers.toString().split(",").join(" ");
+}
+
+function allCourseStudents (id){
+    let courseId = DATABASE.courses[id]
+    let students = []
+    for (let student of allStudents){
+        for (let courses of student.courses){
+            for (let i = 0; i < courses.length; i++){
+                if (courses[i].id == courseId){
+                    students.push(student.map((student) => student.firstName + " " + student.lastName + " " + `(${student.courses[i].passedCredits})`))
+                }
+            }
+        }
+    }
+    return students.toString().split(",").join(" ");
 }
