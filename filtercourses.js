f4a026706193d0b0d3c479073727d2d07e599c89
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-29 12:38:55 +0200

    creating a darkmode switch

--- filtercourses.js
+++ filtercourses.js
@@ -18,3 +18,16 @@ function searchWord (){
 
 document.getElementById("results").addEventListener("keyup", filterCourses)
 
+function toggle(){
+    if (document.getElementById("siteWrapper").style.backgroundColor == "rgb(23, 25, 29)"){
+    document.documentElement.style.setProperty('--WBG', '#teal');
+    document.documentElement.style.setProperty('--DS', '#rgb(0, 97, 97)');
+    document.documentElement.style.setProperty('--WBC', '#goldenrod');
+    document.documentElement.style.setProperty('--BG', '#gainsboro');
+    document.documentElement.style.setProperty('--BC', '#gold');
+    document.documentElement.style.setProperty('--CBG', '#ghostwhite');
+    document.documentElement.style.setProperty('--CBC', '#yellow');
+    document.documentElement.style.setProperty('--CRBG', '#salmon');
+    } else if (document.getElementById("siteWrapper").style.backgroundColor == "teal"){
+    }
+}
