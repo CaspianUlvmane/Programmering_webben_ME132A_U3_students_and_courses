3764c639239cef0f0f6e165c54b69fb01865651b
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-03-30 17:45:32 +0200

    working on dark and light mode

--- filtercourses.js
+++ filtercourses.js
@@ -8,6 +8,15 @@ function filterCourses (){
             resultArray.push(allCourses[i]);
         } 
     }
+    resultArray.sort((a, b) => {
+        if (a.title > b.title){
+            return 1
+        }
+        if (a.title < b.title){
+            return -1
+        }
+        return 0
+    })
     renderAllCourses(resultArray)
 }
 
@@ -19,34 +28,26 @@ function searchWord (){
 document.getElementById("results").addEventListener("keyup", filterCourses)
 
 function toggle(){
-    if (window.getComputedStyle(document.getElementById("siteWrapper")).backgroundColor == "rgb(23, 25, 29)"){
-    document.documentElement.style.setProperty('--WBG', 'teal');
-    document.documentElement.style.setProperty('--DS', 'rgb(0, 97, 97)');
-    document.documentElement.style.setProperty('--WBC', 'goldenrod');
-    document.documentElement.style.setProperty('--BG', 'gainsboro');
-    document.documentElement.style.setProperty('--BC', 'gold');
-    document.documentElement.style.setProperty('--CBG', 'ghostwhite');
-    document.documentElement.style.setProperty('--CBC', 'yellow');
-    document.documentElement.style.setProperty('--CRBG', 'salmon');
-    document.documentElement.style.setProperty('--TC', 'black');
-    document.documentElement.style.setProperty('--DC', 'lightgreen');
-    document.documentElement.style.setProperty('--MC', 'rgba(23, 25, 29, 0)');
+    let darkmode = document.querySelector("#dark")
+    if (darkmode.href = 'dark.css'){
+    darkmode.href = ''
+    localStorage.setItem("theme",  '')
     return
     } 
-    if (window.getComputedStyle(document.getElementById("siteWrapper")).backgroundColor == "rgb(0, 128, 128)"){
-        document.documentElement.style.setProperty('--WBG', 'rgb(23, 25, 29)');
-        document.documentElement.style.setProperty('--DS', 'rgb(11, 17, 17)');
-        document.documentElement.style.setProperty('--WBC', 'rgb(53, 55, 58)');
-        document.documentElement.style.setProperty('--BG', 'gray');
-        document.documentElement.style.setProperty('--BC', 'darkgray');
-        document.documentElement.style.setProperty('--CBG', 'dimgrey');
-        document.documentElement.style.setProperty('--CBC', 'darkgray');
-        document.documentElement.style.setProperty('--CRBG', 'indianred');
-        document.documentElement.style.setProperty('--TC', 'white');
-        document.documentElement.style.setProperty('--DC', 'darkolivegreen');
-        document.documentElement.style.setProperty('--MC', 'rgb(23, 25, 29)');
+    if (darkmode.href == ''){
+       darkmode.href = 'dark.css'
+        localStorage.setItem("theme", 'dark.css')
         return
     }
+
 }
 
 document.getElementById("styling").addEventListener("click", toggle)
+
+function setTheme(){
+    let darkmode = document.querySelector("#dark")
+let Theme = localStorage.getItem("theme")
+    darkmode.href == Theme
+}
+
+setTheme()
--- filterstudents.js
+++ filterstudents.js
@@ -9,6 +9,15 @@ function studentLastname (){
         } 
 
     }
+    studentsArray.sort((a, b) => {
+        if (a.lastName > b.lastName){
+            return 1
+        }
+        if (a.lastName < b.lastName){
+            return -1
+        }
+        return 0
+    })
     renderStudents(studentsArray)
 }
 
@@ -18,3 +27,36 @@ function searchWord (){
 }
 
 document.getElementById("lastname").addEventListener("keyup", studentLastname)
+
+function toggle(){
+    if (window.getComputedStyle(document.getElementById("siteWrapper")).backgroundColor == "rgb(23, 25, 29)"){
+    document.documentElement.style.setProperty('--WBG', 'teal');
+    document.documentElement.style.setProperty('--DS', 'rgb(0, 97, 97)');
+    document.documentElement.style.setProperty('--WBC', 'goldenrod');
+    document.documentElement.style.setProperty('--BG', 'gainsboro');
+    document.documentElement.style.setProperty('--BC', 'gold');
+    document.documentElement.style.setProperty('--CBG', 'ghostwhite');
+    document.documentElement.style.setProperty('--CBC', 'yellow');
+    document.documentElement.style.setProperty('--CRBG', 'salmon');
+    document.documentElement.style.setProperty('--TC', 'black');
+    document.documentElement.style.setProperty('--DC', 'lightgreen');
+    document.documentElement.style.setProperty('--MC', 'rgba(23, 25, 29, 0)');
+    return
+    } 
+    if (window.getComputedStyle(document.getElementById("siteWrapper")).backgroundColor == "rgb(0, 128, 128)"){
+        document.documentElement.style.setProperty('--WBG', 'rgb(23, 25, 29)');
+        document.documentElement.style.setProperty('--DS', 'rgb(11, 17, 17)');
+        document.documentElement.style.setProperty('--WBC', 'rgb(53, 55, 58)');
+        document.documentElement.style.setProperty('--BG', 'gray');
+        document.documentElement.style.setProperty('--BC', 'darkgray');
+        document.documentElement.style.setProperty('--CBG', 'dimgrey');
+        document.documentElement.style.setProperty('--CBC', 'darkgray');
+        document.documentElement.style.setProperty('--CRBG', 'indianred');
+        document.documentElement.style.setProperty('--TC', 'white');
+        document.documentElement.style.setProperty('--DC', 'darkolivegreen');
+        document.documentElement.style.setProperty('--MC', 'rgb(23, 25, 29)');
+        return
+    }
+}
+
+document.getElementById("styling").addEventListener("click", toggle)
